3c5b530544a0afd07d55c2c6d1a4bd3415c038d9
evandixon <evanldixon@live.com>
2015-08-10 19:33:30 -0500

    Added properties for the settings in NDSModSource; Fixed OriginalFilename being set to temporary file

--- ROM Editor/FileFormats/NDSModSource.vb
+++ ROM Editor/FileFormats/NDSModSource.vb
@@ -6,6 +6,96 @@ Namespace FileFormats
         Inherits GenericFile
         Dim _filename As String
         Public Property Settings As Dictionary(Of String, String)
+        Public Property ModName As String
+            Get
+                If Not Settings.ContainsKey("Name") Then
+                    Settings.Add("Name", "")
+                End If
+                Return Settings("Name")
+            End Get
+            Set(ByVal value As String)
+                If Settings.ContainsKey("Name") Then
+                    Settings("Name") = value
+                Else
+                    Settings.Add("Name", value)
+                End If
+            End Set
+        End Property
+        Public Property Author As String
+            Get
+                If Not Settings.ContainsKey("Author") Then
+                    Settings.Add("Author", "")
+                End If
+                Return Settings("Author")
+            End Get
+            Set(ByVal value As String)
+                If Settings.ContainsKey("Author") Then
+                    Settings("Author") = value
+                Else
+                    Settings.Add("Author", value)
+                End If
+            End Set
+        End Property
+        Public Property Description As String
+            Get
+                If Not Settings.ContainsKey("Description") Then
+                    Settings.Add("Description", "")
+                End If
+                Return Settings("Description")
+            End Get
+            Set(ByVal value As String)
+                If Settings.ContainsKey("Description") Then
+                    Settings("Description") = value
+                Else
+                    Settings.Add("Description", value)
+                End If
+            End Set
+        End Property
+        Public Property UpdateURL As String
+            Get
+                If Not Settings.ContainsKey("UpdateURL") Then
+                    Settings.Add("UpdateURL", "")
+                End If
+                Return Settings("UpdateURL")
+            End Get
+            Set(ByVal value As String)
+                If Settings.ContainsKey("UpdateURL") Then
+                    Settings("UpdateURL") = value
+                Else
+                    Settings.Add("UpdateURL", value)
+                End If
+            End Set
+        End Property
+        Public Property DependenciesBefore As String
+            Get
+                If Not Settings.ContainsKey("DependenciesBefore") Then
+                    Settings.Add("DependenciesBefore", "")
+                End If
+                Return Settings("DependenciesBefore")
+            End Get
+            Set(ByVal value As String)
+                If Settings.ContainsKey("DependenciesBefore") Then
+                    Settings("DependenciesBefore") = value
+                Else
+                    Settings.Add("DependenciesBefore", value)
+                End If
+            End Set
+        End Property
+        Public Property DependenciesAfter As String
+            Get
+                If Not Settings.ContainsKey("DependenciesAfter") Then
+                    Settings.Add("DependenciesAfter", "")
+                End If
+                Return Settings("DependenciesAfter")
+            End Get
+            Set(ByVal value As String)
+                If Settings.ContainsKey("DependenciesAfter") Then
+                    Settings("DependenciesAfter") = value
+                Else
+                    Settings.Add("DependenciesAfter", value)
+                End If
+            End Set
+        End Property
 
         Public Overrides Function DefaultExtension() As String
             Return "*.ndsmodsrc"
@@ -22,6 +112,7 @@ Namespace FileFormats
         End Sub
         Public Sub New(Filename As String)
             Me.New
+            Me.OriginalFilename = Filename
             _filename = Filename
             Dim lines = IO.File.ReadAllText(Filename).Split(vbLf)
             For Each item In lines
