1c1e05dc41048f26b74334b99358905c7b672ead
evandixon <evanldixon@live.com>
2015-11-05 08:27:18 -0600

    Implemented iDisposable in NewProjectWindow

--- Sky Editor Base/Windows/NewProjectWindow.xaml.vb
+++ Sky Editor Base/Windows/NewProjectWindow.xaml.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.Windows.Forms
 
 Public Class NewProjectWindow
+    Implements IDisposable
+
     Dim _folderBrowser As FolderBrowserDialog
     Private Sub btnOk_Click(sender As Object, e As RoutedEventArgs) Handles btnOk.Click
         DialogResult = True
@@ -61,4 +63,39 @@ Public Class NewProjectWindow
             ddType.LastSafeValue = value
         End Set
     End Property
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects).
+                If _folderBrowser IsNot Nothing Then
+                    _folderBrowser.Dispose()
+                End If
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        ' TODO: uncomment the following line if Finalize() is overridden above.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
 End Class
