17b77fcf7fb9f0ce10d2940db3c4b291e57a713c
evandixon <evanldixon@live.com>
2015-11-13 10:19:37 -0600

    Added modpack info file type

---
+++ ROM Editor/ModpackInfo.vb
@@ -0,0 +1,10 @@
+﻿Imports SkyEditorBase
+
+Public Class ModpackInfo
+    Public Property Name As String
+    Public Property Author As String
+    Public Property Version As String
+    Public Sub New()
+        Version = "1.0.0"
+    End Sub
+End Class
