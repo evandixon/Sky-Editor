6735280cb24359aa4fd6c4850f2679b152ae50a5
Evan Dixon <evanldixon@live.com>
2016-01-11 09:39:31 -0600

    Revert "LanguageItemList now implements IEnumerable"
    This reverts commit 24eb9d749cc38b9e58c2681585241597c9c9d08c.

--- Sky Editor Base/Code/Language/LanguageItemList.vb
+++ Sky Editor Base/Code/Language/LanguageItemList.vb
@@ -1,6 +1,6 @@
 ﻿Namespace Language
     Public Class LanguageItemList
-        Implements IEnumerable(Of LanguageItem)
+
         Public Property Items As List(Of LanguageItem)
         Public Property Revision As Integer
         Public Sub New()
@@ -8,14 +8,6 @@
             Items = New List(Of LanguageItem)
             Revision = 0
         End Sub
-
-        Public Function GetEnumerator() As IEnumerator(Of LanguageItem) Implements IEnumerable(Of LanguageItem).GetEnumerator
-            Return Items.GetEnumerator
-        End Function
-
-        Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
-            Return Items.GetEnumerator
-        End Function
     End Class
 End Namespace
 
