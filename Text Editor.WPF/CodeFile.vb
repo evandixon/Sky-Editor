5ae8dd364b6c4da69e83d4d3cd4d0414052819d6
Evan Dixon <evanldixon@live.com>
2016-01-21 09:56:51 -0600

    Renamed Text Editor to Text Editor.WPF, since it's mainly a WPF front-end

---
+++ Text Editor.WPF/CodeFile.vb
@@ -0,0 +1,6 @@
+﻿Imports AurelienRibon.Ui.SyntaxHighlightBox
+
+Public Class CodeFile
+    Inherits TextFile
+    Public Overridable ReadOnly Property CodeHighlighter As IHighlighter
+End Class
--- Text Editor/CodeFile.vb
+++
@@ -1,6 +0,0 @@
-﻿Imports AurelienRibon.Ui.SyntaxHighlightBox
-
-Public Class CodeFile
-    Inherits TextFile
-    Public Overridable ReadOnly Property CodeHighlighter As IHighlighter
-End Class
