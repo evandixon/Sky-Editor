434a24dd0e6deeae232d66530f33627d4c01d979
Evan Dixon <evanldixon@live.com>
2016-02-06 17:26:58 -0600

    Added a utility to determine if the current user is an adminsistrator

---
+++ Sky Editor Base/Utilities/CurrentUser.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Security.Principal
+
+Namespace Utilities
+    Public Class CurrentUser
+        Public Shared Function IsAdministrator() As Boolean
+            Dim user = WindowsIdentity.GetCurrent
+            Dim principal As New WindowsPrincipal(user)
+            Return principal.IsInRole(WindowsBuiltInRole.Administrator)
+        End Function
+    End Class
+
+End Namespace
