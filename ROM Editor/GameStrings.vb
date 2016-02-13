05c7ccc497e3bd3409b80a3fd1249d898fc55424
Evan Dixon <evanldixon@live.com>
2016-02-13 16:25:43 -0600

    Added Japanese GTI game code

--- ROM Editor/GameStrings.vb
+++ ROM Editor/GameStrings.vb
@@ -38,7 +38,7 @@ Public Class GameStrings
     Public Const PokemonYCode = "0004000000055E00"
     Public Const ORCode = "000400000011C400"
     Public Const ASCode = "000400000011C500"
-    Public Const GTICode = "00040000000BA(8|9)00"
+    Public Const GTICode = "00040000000(BA8|BA9|950)00"
     Public Const PSMDCode = "0004000000(1746|149B)00"
 
     Public Const SkyCode = "C2S."
