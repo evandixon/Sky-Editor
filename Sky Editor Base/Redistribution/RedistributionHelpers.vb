ed03c61b33b33c4fc5082216492d217739613d8f
Evan Dixon <evanldixon@live.com>
2016-02-16 12:44:22 -0600

    Removed obsolete method

--- Sky Editor Base/Redistribution/RedistributionHelpers.vb
+++ Sky Editor Base/Redistribution/RedistributionHelpers.vb
@@ -19,47 +19,47 @@ Namespace Redistribution
             PluginHelper.Writeline("Distribution preparation complete.")
         End Sub
 
-        ''' <summary>
-        ''' Packages the program and its Resources folder into a single zip file.
-        ''' </summary>
-        ''' <param name="Manager"></param>
-        ''' <param name="ArchiveName"></param>
-        ''' <remarks></remarks>
-        <Obsolete> Public Shared Sub PackProgram(Manager As PluginManager, ArchiveName As String)
-            Dim blacklist As String() = {"DeSmuMe Integration_plg.pdb",
-                                         "DeSmuMe Integration_plg.xml",
-                                         "ICSharpCode.SharpZipLib.dll",
-                                         "ROMEditor_plg.pdb",
-                                         "ROMEditor_plg.xml",
-                                         "SaveEditor.exe",
-                                         "SaveEditor.pdb",
-                                         "SaveEditor.xml",
-                                         "SkyEditor_plg.dll.config",
-                                         "SkyEditor_plg.pdb",
-                                         "SkyEditor_plg.xml"}
+        '''' <summary>
+        '''' Packages the program and its Resources folder into a single zip file.
+        '''' </summary>
+        '''' <param name="Manager"></param>
+        '''' <param name="ArchiveName"></param>
+        '''' <remarks></remarks>
+        '<Obsolete> Public Shared Sub PackProgram(Manager As PluginManager, ArchiveName As String)
+        '    Dim blacklist As String() = {"DeSmuMe Integration_plg.pdb",
+        '                                 "DeSmuMe Integration_plg.xml",
+        '                                 "ICSharpCode.SharpZipLib.dll",
+        '                                 "ROMEditor_plg.pdb",
+        '                                 "ROMEditor_plg.xml",
+        '                                 "SaveEditor.exe",
+        '                                 "SaveEditor.pdb",
+        '                                 "SaveEditor.xml",
+        '                                 "SkyEditor_plg.dll.config",
+        '                                 "SkyEditor_plg.pdb",
+        '                                 "SkyEditor_plg.xml"}
 
-            If Not IO.Directory.Exists(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp")) Then
-                IO.Directory.CreateDirectory(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"))
-            Else
-                IO.Directory.Delete(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"), True)
-                IO.Directory.CreateDirectory(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"))
-            End If
-            If Not IO.Directory.Exists(IO.Path.Combine(Environment.CurrentDirectory, "Sky Editor Archives")) Then
-                IO.Directory.CreateDirectory(IO.Path.Combine(Environment.CurrentDirectory, "Sky Editor Archives"))
-            End If
-            IO.File.Copy(IO.Path.Combine(Environment.CurrentDirectory, "SaveEditor.exe"), IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp", "SaveEditor.exe"), True)
-            IO.File.Copy(IO.Path.Combine(Environment.CurrentDirectory, "ICSharpCode.SharpZipLib.dll"), IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp", "ICSharpCode.SharpZipLib.dll"), True)
-            For Each File In IO.Directory.GetFiles(IO.Path.Combine(Environment.CurrentDirectory, "Resources"), "*", SearchOption.AllDirectories)
-                If Not IO.Directory.Exists(IO.Path.GetDirectoryName(File.Replace(Environment.CurrentDirectory, IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp")))) Then
-                    IO.Directory.CreateDirectory(IO.Path.GetDirectoryName(File.Replace(Environment.CurrentDirectory, IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"))))
-                End If
-                If Not blacklist.Contains(IO.Path.GetFileName(File)) Then
-                    IO.File.Copy(File, File.Replace(Environment.CurrentDirectory, IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp")), True)
-                End If
-            Next
-            Dim z As New FastZip
-            z.CreateZip(IO.Path.Combine(Environment.CurrentDirectory, "Sky Editor Archives", ArchiveName & ".zip"), IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"), True, ".*", ".*")
-        End Sub
+        '    If Not IO.Directory.Exists(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp")) Then
+        '        IO.Directory.CreateDirectory(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"))
+        '    Else
+        '        IO.Directory.Delete(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"), True)
+        '        IO.Directory.CreateDirectory(IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"))
+        '    End If
+        '    If Not IO.Directory.Exists(IO.Path.Combine(Environment.CurrentDirectory, "Sky Editor Archives")) Then
+        '        IO.Directory.CreateDirectory(IO.Path.Combine(Environment.CurrentDirectory, "Sky Editor Archives"))
+        '    End If
+        '    IO.File.Copy(IO.Path.Combine(Environment.CurrentDirectory, "SaveEditor.exe"), IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp", "SaveEditor.exe"), True)
+        '    IO.File.Copy(IO.Path.Combine(Environment.CurrentDirectory, "ICSharpCode.SharpZipLib.dll"), IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp", "ICSharpCode.SharpZipLib.dll"), True)
+        '    For Each File In IO.Directory.GetFiles(IO.Path.Combine(Environment.CurrentDirectory, "Resources"), "*", SearchOption.AllDirectories)
+        '        If Not IO.Directory.Exists(IO.Path.GetDirectoryName(File.Replace(Environment.CurrentDirectory, IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp")))) Then
+        '            IO.Directory.CreateDirectory(IO.Path.GetDirectoryName(File.Replace(Environment.CurrentDirectory, IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"))))
+        '        End If
+        '        If Not blacklist.Contains(IO.Path.GetFileName(File)) Then
+        '            IO.File.Copy(File, File.Replace(Environment.CurrentDirectory, IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp")), True)
+        '        End If
+        '    Next
+        '    Dim z As New FastZip
+        '    z.CreateZip(IO.Path.Combine(Environment.CurrentDirectory, "Sky Editor Archives", ArchiveName & ".zip"), IO.Path.Combine(Environment.CurrentDirectory, "PackageTemp"), True, ".*", ".*")
+        'End Sub
 
         ''' <summary>
         ''' Packs all plugins into one archive per plugin, or only one plugin if PluginName is not nothing.
