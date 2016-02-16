daf4612cf3f0627d94d85df732d06d0ee5a1372f
Evan Dixon <evanldixon@live.com>
2016-02-16 12:47:09 -0600

    Smdh class is now disposable

--- ROM Editor/FileFormats/Smdh.vb
+++ ROM Editor/FileFormats/Smdh.vb
@@ -42,6 +42,7 @@ Namespace FileFormats
         Unused4 = 15
     End Enum
     Public Class SMDH
+        Implements IDisposable
         Private file As FileStream
 
         Private header As New smdhHeader()
@@ -374,6 +375,39 @@ Namespace FileFormats
             writer.Close()
             file.Close()
         End Sub
+
+#Region "IDisposable Support"
+        Private disposedValue As Boolean ' To detect redundant calls
+
+        ' IDisposable
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not disposedValue Then
+                If disposing Then
+                    ' TODO: dispose managed state (managed objects).
+                    SmallIcon.Dispose()
+                End If
+
+                ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+                ' TODO: set large fields to null.
+            End If
+            disposedValue = True
+        End Sub
+
+        ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+        'Protected Overrides Sub Finalize()
+        '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        '    Dispose(False)
+        '    MyBase.Finalize()
+        'End Sub
+
+        ' This code added by Visual Basic to correctly implement the disposable pattern.
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+            Dispose(True)
+            ' TODO: uncomment the following line if Finalize() is overridden above.
+            ' GC.SuppressFinalize(Me)
+        End Sub
+#End Region
     End Class
     '=======================================================
     'Service provided by Telerik (www.telerik.com)
