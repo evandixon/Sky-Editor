840770421e010fcd0adc9dce670e703ad4c2a906
Evan Dixon <evanldixon@live.com>
2016-02-23 08:42:49 -0600

    Added zlib inflate debug console command for future use with PSMD saves

--- Save Editor/ConsoleCommands/Inflate.vb
+++ Save Editor/ConsoleCommands/Inflate.vb
@@ -1,19 +1,21 @@
-﻿Namespace ConsoleCommands
+﻿Imports System.IO.Compression
+
+Namespace ConsoleCommands
     Public Class Inflate
         Inherits SkyEditorBase.ConsoleCommand
 
         Public Overrides Sub Main(Arguments() As String)
-            'If IO.File.Exists(Arguments(0)) Then
-            '    Dim f As New IO.FileStream(Arguments(0), IO.FileMode.Open)
-            '    f.Seek(0, IO.SeekOrigin.Begin)
-            '    Dim z As New Ionic.Zlib.DeflateStream(f, Ionic.Zlib.CompressionMode.Decompress)
-            '    Dim d As New IO.FileStream(Arguments(0) & "-deflated", IO.FileMode.OpenOrCreate)
-            '    d.Seek(0, IO.SeekOrigin.Begin)
-            '    z.CopyTo(d)
-            '    f.Dispose()
-            '    z.Dispose()
-            '    d.Dispose()
-            'End If
+            If IO.File.Exists(Arguments(0)) Then
+                Dim f As New IO.FileStream(Arguments(0), IO.FileMode.Open)
+                f.Seek(2, IO.SeekOrigin.Begin)
+                Dim z As New DeflateStream(f, IO.Compression.CompressionMode.Decompress)
+                Dim d As New IO.FileStream(Arguments(0) & "-deflated", IO.FileMode.OpenOrCreate)
+                d.Seek(0, IO.SeekOrigin.Begin)
+                z.CopyTo(d)
+                f.Dispose()
+                z.Dispose()
+                d.Dispose()
+            End If
         End Sub
 
         Public Overrides ReadOnly Property CommandName As String
