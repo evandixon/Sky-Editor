22a7b1ec6973d0815e1dfe2ce98b7569951d29d3
Evan Dixon <evanldixon@live.com>
2016-03-25 15:23:13 -0500

    Updated assembly version

--- ROM Editor.UI.WPF/My Project/AssemblyInfo.vb
+++ ROM Editor.UI.WPF/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("39f1b4fb-9031-448a-96b4-cebe7e344bb4")> 
+<Assembly: Guid("39f1b4fb-9031-448a-96b4-cebe7e344bb4")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("4.0.1.0")>
+<Assembly: AssemblyFileVersion("4.0.1.0")>
