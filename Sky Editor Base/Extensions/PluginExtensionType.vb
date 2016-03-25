405aeca0e3be5454b0ed83af586aa6f394421943
Evan Dixon <evanldixon@live.com>
2016-03-25 15:24:59 -0500

    Misc changes

--- Sky Editor Base/Extensions/PluginExtensionType.vb
+++ Sky Editor Base/Extensions/PluginExtensionType.vb
@@ -16,11 +16,22 @@ Namespace Extensions
             End Get
         End Property
 
+        Public Overrides Function GetInstalledExtensions() As IEnumerable(Of ExtensionInfo)
+            If SettingsManager.Instance.Settings.DevelopmentMode Then
+                Dim out As New List(Of ExtensionInfo)
+                out.AddRange(MyBase.GetInstalledExtensions)
+                'Todo: load plugins from dev directory to phase out the Plugins directory
+                Return out
+            Else
+                Return MyBase.GetInstalledExtensions
+            End If
+        End Function
+
         Public Overrides Async Function InstallExtension(Extension As ExtensionInfo, TempDir As String) As Task(Of ExtensionInstallResult)
-            Dim potentialAssemblyPaths As New List(Of String)
-            potentialAssemblyPaths.AddRange(IO.Directory.GetFiles(TempDir, "*.dll"))
-            potentialAssemblyPaths.AddRange(IO.Directory.GetFiles(TempDir, "*.exe"))
-            Dim supportedAssemblyPaths = Utilities.ReflectionHelpers.GetSupportedPlugins(potentialAssemblyPaths)
+            'Dim potentialAssemblyPaths As New List(Of String)
+            'potentialAssemblyPaths.AddRange(IO.Directory.GetFiles(TempDir, "*.dll"))
+            'potentialAssemblyPaths.AddRange(IO.Directory.GetFiles(TempDir, "*.exe"))
+            'Dim supportedAssemblyPaths = Utilities.ReflectionHelpers.GetSupportedPlugins(potentialAssemblyPaths)
 
             Await MyBase.InstallExtension(Extension, TempDir)
             Return ExtensionInstallResult.RestartRequired
